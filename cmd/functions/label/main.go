0b5fd5fb00157693e097992c8d67b43bdcb05e89
Arik Kfir <arik@kfirs.com>
2022-03-17 21:25:45 +0200

    Add support for local file mounting
    This change allows functions to request file mounts from the
    workspace. They do this by denoting specific configuration YAML
    paths as mount paths - the values for those YAML paths will specify a
    local file which will be mounted under the container's "/workspace/"
    directory.

--- cmd/functions/label/main.go
+++ cmd/functions/label/main.go
@@ -10,9 +10,22 @@ import (
 
 func main() {
 	pkg.Configure()
+
+	var value string
+	valuePath := viper.GetString("path")
+	if valuePath != "" {
+		bytes, err := os.ReadFile("/workspace/" + valuePath)
+		if err != nil {
+			panic(err)
+		}
+		value = string(bytes)
+	} else {
+		value = viper.GetString("value")
+	}
+
 	pipeline := kio.Pipeline{
 		Inputs:  []kio.Reader{&kio.ByteReader{Reader: os.Stdin}},
-		Filters: []kio.Filter{pkg.Fanout(yaml.SetLabel(viper.GetString("name"), viper.GetString("value")))},
+		Filters: []kio.Filter{pkg.Fanout(yaml.SetLabel(viper.GetString("name"), value))},
 		Outputs: []kio.Writer{kio.ByteWriter{Writer: os.Stdout}},
 	}
 	if err := pipeline.Execute(); err != nil {
